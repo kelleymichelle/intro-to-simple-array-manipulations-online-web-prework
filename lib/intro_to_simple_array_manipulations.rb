8c40e5b258a2903de29fa77c60492e20b7acfa03
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-13 16:41:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+
+
+def using_push(array, string)
+  array = countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  string = next_country = "Niger"
+    countries_in_western_africa.push(next_country)
+end
+
+def using_unshift(array, string)
+  array = neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  string = new_neighborhood = "Brooklyn Heights"
+    neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+end
+
+def using_pop
+  
+end
+
+def shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end  
